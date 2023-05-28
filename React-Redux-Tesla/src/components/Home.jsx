a0d25a15081bd9e96633ef85f57138989f068a71
Jayy2023 <119895456+Jayy2023@users.noreply.github.com>
2023-05-28 12:26:26 -0400

    added all the sections for tesla app

---
+++ React-Redux-Tesla/src/components/Home.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import styled from "styled-components"
+import Section from './Section'
+import jordanTeslaImage from './image/jordanTesla.jpg';
+
+
+
+function Home() {
+  return (
+    <Container>
+    <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+    />
+    <Section 
+          title="Model Y"
+          description="Order Online for Touchless Delivery"
+          backgroundImg="model-y.jpg"
+          leftBtnText="Custom order"
+          rightBtnText="Existing inventory"
+    />
+    <Section 
+          title="Model X"
+          description="Order Online for Touchless Delivery"
+          backgroundImg="model-x.jpg"
+          leftBtnText="Custom order"
+          rightBtnText="Existing inventory"
+    />
+    <Section 
+          title="Model 3 "
+          description="Order Online for Touchless Delivery"
+          backgroundImg="model-3.jpg"
+          leftBtnText="Custom order"
+          rightBtnText="Existing inventory"
+    />
+     <Section 
+          title="Meet Your Creator"
+          description=" Hey my name is Jordan Couch a developer who created a React-based Tesla app. 
+          This app enables users to make custom orders or view the existing inventory of Tesla vehicles." 
+          backgroundImg="Tesla.jpg"
+          leftBtnText="Portfolio"
+          sideImg={jordanTeslaImage}
+    />
+    </Container>
+  )
+}
+
+export default Home
+const Container = styled.div`
+height: 100vh;
+`
